a8ffe21b4a4ac5e3b8b206fac19ae26f781c806e
Jonathan McPherson <jonathan@rstudio.com>
2015-09-01 19:12:38 -0700

    add a few comments & validation

--- circularprimes.go
+++ circularprimes.go
@@ -6,45 +6,64 @@ import (
 	"strconv"
 )
 
+// given a number, "rotates" the number by one by moving the last digit into
+// the position of the first digit: e.g. 337 -> 733
 func rotate(i float64) float64 {
 	return math.Mod(i, 10)*(math.Pow(10, math.Floor(math.Log10(i)))) + math.Floor(i/10)
 }
 
-func sieve(num int64, nums []bool) {
-	for i := num * 2; i < int64(len(nums)); i += num {
-		nums[i] = false
+// performs the "sieve of Erostothanes" for a single number
+func sieve(num int64, primes []bool) {
+	for i := num * 2; i < int64(len(primes)); i += num {
+		primes[i] = false
 	}
 }
 
 func main() {
+	// validate arguments
+	if len(os.Args) < 2 {
+		println("usage: circularprimes.go NUM")
+		return
+	}
+
+	// make an array and mark all numbers "prime"
 	num, _ := strconv.ParseFloat(os.Args[1], 10)
-	nums := make([]bool, int64(num))
-	for i := range nums {
-		nums[i] = true
+	primes := make([]bool, int64(num))
+	for i := range primes {
+		primes[i] = true
 	}
+
+	// mark all non-prime numbers
 	for i := 2; i <= int(math.Sqrt(num)); i++ {
-		sieve(int64(i), nums)
+		sieve(int64(i), primes)
 	}
+
+	// find primes and check to see if all their rotations are also prime
 	circularprimes := 0
-	for i := range nums {
-		if i > 2 && nums[i] {
+	for i := range primes {
+		if i > 2 && primes[i] {
+			// this prime might be circular
 			candidate := float64(i)
 			ok := true
 			desc := strconv.FormatInt(int64(i), 10)
+
+			// check all its rotations (1 rotation for 2-digit numbers,
+			// 2 rotations for 3-digit numbers, etc.)
 			for j := 0; j < int(math.Floor(math.Log10(float64(i)))); j++ {
 				candidate = rotate(candidate)
-				if (candidate < num) && !nums[int(candidate)] {
+				if (candidate < num) && !primes[int(candidate)] {
 					// this rotation is not prime
 					ok = false
 					break
-				} else {
+				} else if candidate < num {
 					// this rotation is prime, so mark the prime as visited
-					nums[int(candidate)] = false
+					primes[int(candidate)] = false
 					desc += ", "
 					desc += strconv.FormatInt(int64(candidate), 10)
 				}
 			}
 
+			// if all rotation were prime, output the number
 			if ok {
 				println(desc)
 				circularprimes++
